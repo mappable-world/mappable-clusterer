91e9f137dd65174772977bbe3c2034f5d6660fbf
xdan <chupurnov@gmail.com>
2023-09-18 21:04:34 +0300

    Added github pages

--- example/common.js
+++ example/common.js
@@ -11,29 +11,35 @@ mappable.import.loaders.unshift(async (pkg) => {
 
     Object.assign(mappable, window[`${pkg}`]);
     return window[`${pkg}`];
-})
-
+});
 
 const BOUNDS = [
-    [54.58311, 25.99850],
-    [56.30248, 24.47889]
+    [-73.95184497632046, 40.71517531718148],
+    [-73.79379690161176, 40.676480480724436]
 ];
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const LOCATION = {bounds: BOUNDS};
 
-const seed = s => () => {s = Math.sin(s) * 10000; return s - Math.floor(s);}
+const seed = (s) => () => {
+    s = Math.sin(s) * 10000;
+    return s - Math.floor(s);
+};
 
 const rnd = seed(10000); // () => Math.random()
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const rndNum = () => Math.floor(Math.random() * (1000 - 1)) + 1;
-rnd.point = (bounds = BOUNDS) => [bounds[0][0] + (bounds[1][0] - bounds[0][0]) * rnd(), bounds[1][1] + (bounds[0][1] - bounds[1][1]) * rnd()];
+rnd.point = (bounds = BOUNDS) => [
+    bounds[0][0] + (bounds[1][0] - bounds[0][0]) * rnd(),
+    bounds[1][1] + (bounds[0][1] - bounds[1][1]) * rnd()
+];
 
 let i = 0;
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const getRandomPoints = (count, bounds) => Array.from({length: count}, () => ({
-    type: 'Feature',
-    id: i++,
-    geometry: {coordinates: rnd.point(bounds)},
-    properties: {name: 'beer shop'}
-}));
+const getRandomPoints = (count, bounds) =>
+    Array.from({length: count}, () => ({
+        type: 'Feature',
+        id: i++,
+        geometry: {coordinates: rnd.point(bounds)},
+        properties: {name: 'beer shop'}
+    }));
